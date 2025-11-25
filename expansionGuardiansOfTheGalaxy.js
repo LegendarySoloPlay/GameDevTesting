3d086f961b69ec6cc589f5d67009174f3882ad17
LegendarySoloPlay <legendarysoloplay@gmail.com>
2025-11-25 20:23:34 +1100

    GotG Planning

---
+++ expansionGuardiansOfTheGalaxy.js
@@ -0,0 +1,84 @@
+// Expansion - Guardians of the Galaxy
+// 25.11.25 20.20
+
+// Heroes
+
+function draxTheDestroyerKnivesOfTheHunter() {
+
+}
+
+function draxTheDestroyerInterstellarTracker() {
+
+}
+
+function draxTheDestroyerTheDestroyer() {
+
+}
+
+function draxTheDestroyerAvatarOfDestruction() {
+
+}
+
+function gamoraBountyHunter() {
+
+}
+
+function gamoraDeadliestWomanInTheUniverse() {
+
+}
+
+function gamoraGalacticAssassin() {
+
+}
+
+function gamoraGodslayerBlade() {
+
+}
+
+function grootSurvivingSprig() {
+
+}
+
+function grootPruneTheGrowths() {
+
+}
+
+function grootGrootAndBranches() {
+
+}
+
+function grootIAmGroot() {
+
+}
+
+function rocketRaccoonGrittyScavenger() {
+
+}
+
+function rocketRaccoonTriggerHappy() {
+
+}
+
+function rocketRaccoonIncomingDetector() {
+
+}
+
+function rocketRaccoonVengeanceIsRocket() {
+
+}
+
+function starLordElementGuns() {
+
+}
+
+function starLordLegendaryOutlaw() {
+
+}
+
+function starLordImplacedMemoryChip() {
+
+}
+
+function starLordSentientStarship() {
+
+}
